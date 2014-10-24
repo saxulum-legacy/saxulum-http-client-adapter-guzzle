a0e59fa29911ad04a019e39aecd14b3de695afd3
Dominik Zogg <dominik.zogg@gmail.com>
2014-10-24 21:30:48 +0200

    add more tests and fix adapter

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -5,6 +5,7 @@ namespace Saxulum\HttpClient\Guzzle;
 use GuzzleHttp\Client;
 use GuzzleHttp\Message\Request as GuzzleRequest;
 use GuzzleHttp\Message\Response as GuzzleResponse;
+use GuzzleHttp\Stream\Stream;
 use Saxulum\HttpClient\HeaderConverter;
 use Saxulum\HttpClient\HttpClientInterface;
 use Saxulum\HttpClient\Request;
@@ -56,9 +57,27 @@ class HttpClient implements HttpClientInterface
             )
         );
 
-        $guzzleRequest->setHeaders($request->getHeaders());
-        $guzzleRequest->setBody($request->getContent());
+        if (null !== $content = $request->getContent()) {
+            $guzzleRequest->setBody(Stream::factory($content));
+        }
+
+        $guzzleRequest->setHeaders($this->prepareHeaders($request));
 
         return $guzzleRequest;
+    }
+
+    /**
+     * @param  Request $request
+     * @return array
+     */
+    protected function prepareHeaders(Request $request)
+    {
+        $headers = $request->getHeaders();
+
+        if (null !== $request->getContent() && !isset($headers['Content-Type'])) {
+            $headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        }
+
+        return $headers;
     }
 }
